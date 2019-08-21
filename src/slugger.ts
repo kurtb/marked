d4516146af01c02fdb932c92fb1a4165e2b5ef96
Kurt Berglund <kurtb@microsoft.com>
2019-08-20 18:56:27 -0700

    Port slugger to TS

--- src/slugger.ts
+++ src/slugger.ts
@@ -2,29 +2,29 @@
  * Slugger generates header id
  */
 
-function Slugger() {
-    this.seen = {};
+class Slugger {
+    public seen = {};
+
+    /**
+     * Convert string to unique id
+     */
+
+    public slug = function (value) {
+        var slug = value
+            .toLowerCase()
+            .trim()
+            .replace(/[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,./:;<=>?@[\]^`{|}~]/g, '')
+            .replace(/\s/g, '-');
+
+        if (this.seen.hasOwnProperty(slug)) {
+            var originalSlug = slug;
+            do {
+                this.seen[originalSlug]++;
+                slug = originalSlug + '-' + this.seen[originalSlug];
+            } while (this.seen.hasOwnProperty(slug));
+        }
+        this.seen[slug] = 0;
+
+        return slug;
+    };
 }
-
-/**
- * Convert string to unique id
- */
-
-Slugger.prototype.slug = function (value) {
-    var slug = value
-        .toLowerCase()
-        .trim()
-        .replace(/[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,./:;<=>?@[\]^`{|}~]/g, '')
-        .replace(/\s/g, '-');
-
-    if (this.seen.hasOwnProperty(slug)) {
-        var originalSlug = slug;
-        do {
-            this.seen[originalSlug]++;
-            slug = originalSlug + '-' + this.seen[originalSlug];
-        } while (this.seen.hasOwnProperty(slug));
-    }
-    this.seen[slug] = 0;
-
-    return slug;
-};
