2efe5f47f1069ccae674af9aa7e01fee7f2917ff
Kurt Berglund <kurtberglund@hotmail.com>
2019-08-24 17:44:46 -0700

    Port renderer and escape

--- src/escape.ts
+++ src/escape.ts
@@ -1,26 +1,28 @@
-function escape(html, encode) {
-    if (encode) {
-        if (escape.escapeTest.test(html)) {
-            return html.replace(escape.escapeReplace, function (ch) { return escape.replacements[ch]; });
+export const escape = Object.assign(
+    function(html, encode?) {
+        if (encode) {
+            if (escape.escapeTest.test(html)) {
+                return html.replace(escape.escapeReplace, function (ch) { return escape.replacements[ch]; });
+            }
+        } else {
+            if (escape.escapeTestNoEncode.test(html)) {
+                return html.replace(escape.escapeReplaceNoEncode, function (ch) { return escape.replacements[ch]; });
+            }
         }
-    } else {
-        if (escape.escapeTestNoEncode.test(html)) {
-            return html.replace(escape.escapeReplaceNoEncode, function (ch) { return escape.replacements[ch]; });
-        }
+    
+        return html;
+    },
+    {
+        escapeTest: /[&<>"']/,
+        escapeReplace: /[&<>"']/g,
+        replacements: {
+            '&': '&amp;',
+            '<': '&lt;',
+            '>': '&gt;',
+            '"': '&quot;',
+            "'": '&#39;'
+        },
+        escapeTestNoEncode: /[<>"']|&(?!#?\w+;)/,
+        escapeReplaceNoEncode: /[<>"']|&(?!#?\w+;)/g,
     }
-
-    return html;
-}
-
-escape.escapeTest = /[&<>"']/;
-escape.escapeReplace = /[&<>"']/g;
-escape.replacements = {
-    '&': '&amp;',
-    '<': '&lt;',
-    '>': '&gt;',
-    '"': '&quot;',
-    "'": '&#39;'
-};
-
-escape.escapeTestNoEncode = /[<>"']|&(?!#?\w+;)/;
-escape.escapeReplaceNoEncode = /[<>"']|&(?!#?\w+;)/g;
+);
--- src/renderer.ts
+++ src/renderer.ts
@@ -2,156 +2,164 @@
  * Renderer
  */
 
-function Renderer(options) {
-    this.options = options || marked.defaults;
-}
+import { escape } from "./escape";
+import { cleanUrl } from "./utils";
+import * as marked from "./marked";
 
-Renderer.prototype.code = function (code, infostring, escaped) {
-    var lang = (infostring || '').match(/\S*/)[0];
-    if (this.options.highlight) {
-        var out = this.options.highlight(code, lang);
-        if (out != null && out !== code) {
-            escaped = true;
-            code = out;
+export class Renderer {
+    private options;
+
+    constructor(options?) {
+        this.options = options || marked.defaults;
+    }
+
+    public code(code, infostring, escaped) {
+        var lang = (infostring || '').match(/\S*/)[0];
+        if (this.options.highlight) {
+            var out = this.options.highlight(code, lang);
+            if (out != null && out !== code) {
+                escaped = true;
+                code = out;
+            }
         }
-    }
 
-    if (!lang) {
-        return '<pre><code>'
-            + (escaped ? code : escape(code, true))
-            + '</code></pre>';
-    }
+        if (!lang) {
+            return '<pre><code>'
+                + (escaped ? code : escape(code, true))
+                + '</code></pre>';
+        }
 
-    return '<pre><code class="'
-        + this.options.langPrefix
-        + escape(lang, true)
-        + '">'
-        + (escaped ? code : escape(code, true))
-        + '</code></pre>\n';
-};
-
-Renderer.prototype.blockquote = function (quote) {
-    return '<blockquote>\n' + quote + '</blockquote>\n';
-};
-
-Renderer.prototype.html = function (html) {
-    return html;
-};
-
-Renderer.prototype.heading = function (text, level, raw, slugger) {
-    if (this.options.headerIds) {
-        return '<h'
-            + level
-            + ' id="'
-            + this.options.headerPrefix
-            + slugger.slug(raw)
+        return '<pre><code class="'
+            + this.options.langPrefix
+            + escape(lang, true)
             + '">'
-            + text
-            + '</h'
-            + level
-            + '>\n';
-    }
-    // ignore IDs
-    return '<h' + level + '>' + text + '</h' + level + '>\n';
-};
+            + (escaped ? code : escape(code, true))
+            + '</code></pre>\n';
+    };
 
-Renderer.prototype.hr = function () {
-    return this.options.xhtml ? '<hr/>\n' : '<hr>\n';
-};
+    public blockquote(quote) {
+        return '<blockquote>\n' + quote + '</blockquote>\n';
+    };
 
-Renderer.prototype.list = function (body, ordered, start) {
-    var type = ordered ? 'ol' : 'ul',
-        startatt = (ordered && start !== 1) ? (' start="' + start + '"') : '';
-    return '<' + type + startatt + '>\n' + body + '</' + type + '>\n';
-};
+    public html(html) {
+        return html;
+    };
 
-Renderer.prototype.listitem = function (text) {
-    return '<li>' + text + '</li>\n';
-};
+    public heading(text, level, raw, slugger) {
+        if (this.options.headerIds) {
+            return '<h'
+                + level
+                + ' id="'
+                + this.options.headerPrefix
+                + slugger.slug(raw)
+                + '">'
+                + text
+                + '</h'
+                + level
+                + '>\n';
+        }
+        // ignore IDs
+        return '<h' + level + '>' + text + '</h' + level + '>\n';
+    };
 
-Renderer.prototype.checkbox = function (checked) {
-    return '<input '
-        + (checked ? 'checked="" ' : '')
-        + 'disabled="" type="checkbox"'
-        + (this.options.xhtml ? ' /' : '')
-        + '> ';
-};
+    public hr() {
+        return this.options.xhtml ? '<hr/>\n' : '<hr>\n';
+    };
 
-Renderer.prototype.paragraph = function (text) {
-    return '<p>' + text + '</p>\n';
-};
+    public list(body, ordered, start) {
+        var type = ordered ? 'ol' : 'ul',
+            startatt = (ordered && start !== 1) ? (' start="' + start + '"') : '';
+        return '<' + type + startatt + '>\n' + body + '</' + type + '>\n';
+    };
 
-Renderer.prototype.table = function (header, body) {
-    if (body) body = '<tbody>' + body + '</tbody>';
+    public listitem(text) {
+        return '<li>' + text + '</li>\n';
+    };
 
-    return '<table>\n'
-        + '<thead>\n'
-        + header
-        + '</thead>\n'
-        + body
-        + '</table>\n';
-};
+    public checkbox(checked) {
+        return '<input '
+            + (checked ? 'checked="" ' : '')
+            + 'disabled="" type="checkbox"'
+            + (this.options.xhtml ? ' /' : '')
+            + '> ';
+    };
 
-Renderer.prototype.tablerow = function (content) {
-    return '<tr>\n' + content + '</tr>\n';
-};
+    public paragraph(text) {
+        return '<p>' + text + '</p>\n';
+    };
 
-Renderer.prototype.tablecell = function (content, flags) {
-    var type = flags.header ? 'th' : 'td';
-    var tag = flags.align
-        ? '<' + type + ' align="' + flags.align + '">'
-        : '<' + type + '>';
-    return tag + content + '</' + type + '>\n';
-};
+    public table(header, body) {
+        if (body) body = '<tbody>' + body + '</tbody>';
 
-// span level renderer
-Renderer.prototype.strong = function (text) {
-    return '<strong>' + text + '</strong>';
-};
+        return '<table>\n'
+            + '<thead>\n'
+            + header
+            + '</thead>\n'
+            + body
+            + '</table>\n';
+    };
 
-Renderer.prototype.em = function (text) {
-    return '<em>' + text + '</em>';
-};
+    public tablerow(content) {
+        return '<tr>\n' + content + '</tr>\n';
+    };
 
-Renderer.prototype.codespan = function (text) {
-    return '<code>' + text + '</code>';
-};
+    public tablecell(content, flags) {
+        var type = flags.header ? 'th' : 'td';
+        var tag = flags.align
+            ? '<' + type + ' align="' + flags.align + '">'
+            : '<' + type + '>';
+        return tag + content + '</' + type + '>\n';
+    };
 
-Renderer.prototype.br = function () {
-    return this.options.xhtml ? '<br/>' : '<br>';
-};
+    // span level renderer
+    public strong(text) {
+        return '<strong>' + text + '</strong>';
+    };
 
-Renderer.prototype.del = function (text) {
-    return '<del>' + text + '</del>';
-};
+    public em(text) {
+        return '<em>' + text + '</em>';
+    };
 
-Renderer.prototype.link = function (href, title, text) {
-    href = cleanUrl(this.options.sanitize, this.options.baseUrl, href);
-    if (href === null) {
+    public codespan(text) {
+        return '<code>' + text + '</code>';
+    };
+
+    public br() {
+        return this.options.xhtml ? '<br/>' : '<br>';
+    };
+
+    public del(text) {
+        return '<del>' + text + '</del>';
+    };
+
+    public link(href, title, text) {
+        href = cleanUrl(this.options.sanitize, this.options.baseUrl, href);
+        if (href === null) {
+            return text;
+        }
+        var out = '<a href="' + escape(href) + '"';
+        if (title) {
+            out += ' title="' + title + '"';
+        }
+        out += '>' + text + '</a>';
+        return out;
+    };
+
+    public image(href, title, text) {
+        href = cleanUrl(this.options.sanitize, this.options.baseUrl, href);
+        if (href === null) {
+            return text;
+        }
+
+        var out = '<img src="' + href + '" alt="' + text + '"';
+        if (title) {
+            out += ' title="' + title + '"';
+        }
+        out += this.options.xhtml ? '/>' : '>';
+        return out;
+    };
+
+    public text(text) {
         return text;
-    }
-    var out = '<a href="' + escape(href) + '"';
-    if (title) {
-        out += ' title="' + title + '"';
-    }
-    out += '>' + text + '</a>';
-    return out;
-};
-
-Renderer.prototype.image = function (href, title, text) {
-    href = cleanUrl(this.options.sanitize, this.options.baseUrl, href);
-    if (href === null) {
-        return text;
-    }
-
-    var out = '<img src="' + href + '" alt="' + text + '"';
-    if (title) {
-        out += ' title="' + title + '"';
-    }
-    out += this.options.xhtml ? '/>' : '>';
-    return out;
-};
-
-Renderer.prototype.text = function (text) {
-    return text;
-};
+    };
+}
